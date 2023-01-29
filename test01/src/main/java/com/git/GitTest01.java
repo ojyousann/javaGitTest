e6e1b6ddf03b567780208aa963242d2f37ac3e39
wzy <wzy@bb.com>
2023-01-29 19:16:36 +0800

    push提交

--- test01/src/main/java/com/git/GitTest01.java
+++ test01/src/main/java/com/git/GitTest01.java
@@ -8,5 +8,6 @@ public class GitTest01 {
         System.out.println("hello git 4");
         System.out.println("master test");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
